9f24a4d94477e1abbc5ba969e55a2bf8ee6b897f
malini <malini@DESKTOP-ODS1K3U>
2025-09-18 15:05:47 +0530

    Basic Code

--- SystemDesign/src/com/wipro/bank/main/MainClass.java
+++ SystemDesign/src/com/wipro/bank/main/MainClass.java
@@ -1,27 +1,23 @@
 package com.wipro.bank.main;
 
-import java.util.Scanner;
 import com.wipro.bank.service.BankService;
 
 public class MainClass {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         BankService service = new BankService();
 
-        System.out.print("Enter Amount: ");
-        float principal = sc.nextFloat();
+        System.out.println("Test Case 1\n ");
+        service.calculate(500, 5, 20, "male"); 
+        System.out.println("Test Case 2 \n");
+        service.calculate(1200, 10, 65, "female"); 
 
-        System.out.print("Enter Tenure (5 or 10 years): ");
-        int tenure = sc.nextInt();
+        System.out.println("Test Case 3 \n");
+        service.calculate(2500, 5, 59, "female"); 
 
-        System.out.print("Enter Age: ");
-        int age = sc.nextInt();
+        System.out.println("Test Case 4\n ");
+        service.calculate(10000, 10, 60, "male"); 
 
-        System.out.print("Enter Gender: ");
-        String gender = sc.next();
-
-        service.calculate(principal, tenure, age, gender);
-
-        sc.close();
+        System.out.println("Test Case 5 \n");
+        service.calculate(750, 5, 100, "male"); 
     }
 }
